1ea49ef07aee0581f9e150fc836321739ca1ec1a
dorian <doriantheblue@naver.com>
2024-01-09 21:52:32 +0900

    feat: 관람객 구현, 관람객은 가방을 소지할 수 있다.

---
+++ src/main/java/com/example/object/_01/Audience.java
@@ -0,0 +1,13 @@
+package com.example.object._01;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
