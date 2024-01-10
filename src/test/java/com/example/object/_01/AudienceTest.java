f77cbd48ef4e5bca98da6d67a441ac5ccd320264
dorian <doriantheblue@naver.com>
2024-01-10 22:22:27 +0900

    test: Audience Test 추가

---
+++ src/test/java/com/example/object/_01/AudienceTest.java
@@ -0,0 +1,32 @@
+package com.example.object._01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AudienceTest {
+
+    @Test
+    @DisplayName("초대장이 있는 경우 0원을 지불하고 가방엔 티켓이 추가된다.")
+    public void hasInvitation() {
+        Bag bag = new Bag(new Invitation(), 1000L);
+        Audience audience = new Audience(bag);
+        Long fee = audience.buy(new Ticket());
+
+        assertEquals(0L, fee);
+        assertTrue(bag.hasTicket());
+    }
+
+    @Test
+    @DisplayName("초대장이 없는 경우 티켓값을 지불하고 가방엔 티켓이 추가된다.")
+    public void noInvitation() {
+        Bag bag = new Bag(null, 1000L);
+        Audience audience = new Audience(bag);
+        Long fee = audience.buy(new Ticket());
+
+        assertEquals(1000L, fee);
+        assertTrue(bag.hasTicket());
+    }
+}
