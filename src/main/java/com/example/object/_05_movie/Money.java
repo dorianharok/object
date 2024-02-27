ca85ca45512aa61137877f5c2253756aeea64ba3
dorian <doriantheblue@naver.com>
2024-02-27 13:24:52 +0900

    feat: Movie 구현

--- src/main/java/com/example/object/_05_movie/Money.java
+++
@@ -1,6 +0,0 @@
-package com.example.object._05_movie;
-
-public class Money {
-    public Money times(int audienceCount) {
-    }
-}
--- src/main/java/com/example/object/_05_movie/Movie.java
+++ src/main/java/com/example/object/_05_movie/Movie.java
@@ -1,7 +1,43 @@
 package com.example.object._05_movie;
 
+import com.example.object._02.Money;
+
+import java.time.Duration;
+import java.util.List;
+
 public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
     public Money calculateMovieFee(Screening screening) {
-        return new Money();
+        switch(movieType) {
+            case AMOUNT_DISCOUNT -> calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT -> calculatePercentDiscountAmount();
+            case NONE_DISCOUNT -> calculateNoneDiscountAmount();
+        }
+
+        throw new IllegalStateException();
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream().anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 }
